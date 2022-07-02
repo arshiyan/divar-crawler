15e38f386753fd96e64e743e065920edc362f485
Mohammad Javad Arshiyan <mohammadjavad_arshian@aqrazavi.org>
2022-07-02 15:14:24 +0430

    update code & test

--- robo.py
+++ robo.py
@@ -10,7 +10,7 @@ import json
 from datetime import datetime
 
 
-# 2)4LSuyFNb.4
+
 
 def run_every():
     threading.Timer(random.randint(60.0, 300.0), run_every).start() # called every minute
@@ -37,7 +37,7 @@ def run_every():
 
     city_name = get_city_function()
     
-    print(city_name)
+    print("start fetch")
     
     DIVAR_BASE_URL = 'https://divar.ir'
     # divar shiraz
@@ -46,7 +46,7 @@ def run_every():
         DIVAR_BASE_URL + '/s/'+city_name)
     divar_soup = bs4.BeautifulSoup(divar.content, 'lxml')
 
-    print(DIVAR_BASE_URL + '/s/'+city_name+'/auto')
+    #print(DIVAR_BASE_URL + '/s/'+city_name+'/auto')
     
     #sys.exit()
     results = divar_soup.select('.post-card-item')
@@ -69,68 +69,70 @@ def run_every():
         soup = BeautifulSoup(product.text, 'html.parser')  
         
         #soup = soup.select('<div>',attrs={'class':'kt-carousel post-image-slider kt-carousel--padded'})
-        results = soup.select('img', attrs={'class':'kt-image-block__image'})
+        #results = soup.select('img', attrs={'class':'kt-image-block__image'})
         
         records = []  
         str = "divarcdn"
-        for result in results:  
-            if str in result['src']:
-                records.append(result['src'])
+        #for result in results:  
+            #if str in result['src']:
+               # records.append(result['src'])
             
 
 
         #print('images:  ')
         #print(records)
 
-        if(len(records) > 0 ):
+        #if(len(records) > 0 ):
             #print(x)
 
-            title = x[0]
-            category = x[1]
-            city = x[2]
-            province = city_name
-            #city = x[2].split('،')[1]
-            #province = x[2].split('،')[0]
-            patterns = link_to_product.split('/')
-            code =format(patterns[-1])
-            values = soup.select('div', attrs={'class':'post-info'})
-            print(values)
-            exit()
-            for value in values:
-                print(value)
-                print("ddddddd")
+        title = x[0]
+        category = x[1]
+        city = x[2]
+        province = city_name
+        #city = x[2].split('،')[1]
+        #province = x[2].split('،')[0]
+        patterns = link_to_product.split('/')
+        code =format(patterns[-1])
+        
 
-            headers = {
-                "Host": "api.divar.ir",
-                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:100.0) Gecko/20100101 Firefox/100.0",
-                "Accept": "application/json, text/plain, */*",
-                "Accept-Language": "en-US,en;q=0.5",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Referer": "https://divar.ir/",
-                "Authorization": "Basic eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzODQyMjc0NzciLCJpc3MiOiJhdXRoIiwiaWF0IjoxNjUzOTI1MTk5LCJleHAiOjE2NTUyMjExOTksInZlcmlmaWVkX3RpbWUiOjE2NTM5MjUxOTksInVzZXItdHlwZSI6InBlcnNvbmFsIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYzNFx1MDYyZVx1MDYzNVx1MDZjYyIsInNpZCI6ImQ4ZDFlMDJlLWQwYmMtNDM3Ny1hNjliLTliMzUyZTVlNmYzMiJ9.DaUStkE2XCelkKrR-n3RUb8akXBmYxUTJ1Xdmepjykc",
-                "Origin": "https://divar.ir",
-                "Connection": "keep-alive",
-                "Sec-Fetch-Dest": "empty",
-                "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-site",
-            }
-            phone_number = requests.get('https://api.divar.ir/v5/posts/'+(code)+'/contact/',headers=headers)
-            #print(phone_number.json())
-            phone = phone_number.json()['widgets']['contact']['phone']
-            if phone and phone.strip():
-                items.append( (title, category,city,phone,code,json.dumps(records)) );
-                """
-                except NameError:
-                    phone_number.json()['error']:
-                    print("there is an error for fetching")
-                    time.sleep(random.randint(600.0, 1200.0))
-                """
-                mycursor = mydb.cursor()
-                sql = "INSERT INTO ads (title,category,city,phone,code,images) VALUES (%s, %s, %s, %s, %s, %s)"
-                mycursor.executemany(sql, items)
+        headers = {
+            "Host": "api.divar.ir",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:100.0) Gecko/20100101 Firefox/100.0",
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": "https://divar.ir/",
+            "Authorization": "Basic eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzODQyMjc0NzciLCJpc3MiOiJhdXRoIiwiaWF0IjoxNjU2NzU4MDA5LCJleHAiOjE2NTgwNTQwMDksInZlcmlmaWVkX3RpbWUiOjE2NTY3NTgwMDksInVzZXItdHlwZSI6InBlcnNvbmFsIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYzNFx1MDYyZVx1MDYzNVx1MDZjYyIsInNpZCI6ImVhN2Q5OTI0LTI3ZDQtNDRmOS05OGY5LTBiZjNkN2IzNGFhMSJ9.tY7bF-ixMP5ajwZoLeSU5eTobFpLkSN8giiX61t0IDc",
+            "Origin": "https://divar.ir",
+            "Connection": "keep-alive",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+        }
+        phone_number = requests.get('https://api.divar.ir/v5/posts/'+(code)+'/contact/',headers=headers)
+        #print(phone_number.json())
+        phone = phone_number.json()['widgets']['contact']['phone']
+        
+        print('Title: '+title)
+        print('Category: '+category)
+        print('City: '+city)
+        print('Phone: '+phone)
+        print('Code: '+code) 
 
-                mydb.commit()
-                mydb.close()
+        if phone and phone.strip():
+            items.append( (title, category,city,phone,code,json.dumps(records)) );
+            """
+            except NameError:
+                phone_number.json()['error']:
+                print("there is an error for fetching")
+                time.sleep(random.randint(600.0, 1200.0))
+            """
+            mycursor = mydb.cursor()
+            sql = "INSERT INTO ads (title,category,city,phone,code,images) VALUES (%s, %s, %s, %s, %s, %s)"
+            mycursor.executemany(sql, items)
+
+            mydb.commit()
+            mydb.close()
         time.sleep(random.randint(60.0, 300.0))
 
     """
